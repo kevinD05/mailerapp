f2524764b453c97b356d53702aa7813bf4efbe51
unknown <alexanderkevindiaz05@gmail.com>
2022-09-20 14:23:08 -0600

    html

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,6 @@ def create_app():
     db.init_app(app)
 
     from . import mail
-
     app.register_blueprint(mail.bp)
 
     return app
--- app/mail.py
+++ app/mail.py
@@ -1,12 +1,10 @@
-from flask import(
+from flask import (
     Blueprint, render_template
 )
+
 
 bp = Blueprint('mail', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('mails/index.html')
-    
-   
-    
