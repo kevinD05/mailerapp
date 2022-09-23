78b258e6fab06d7b74a91f1ddce20a6bfe8b45db
unknown <alexanderkevindiaz05@gmail.com>
2022-09-23 14:25:03 -0600

    create

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash, redirect, url_for
 )
 
 from app.db import get_db
@@ -13,5 +13,34 @@ def index():
     c.execute('SELECT * FROM email')
     mails =c.fetchall()
 
-    print(mails)
+    
     return render_template('mails/index.html', mails=mails )
+
+@bp.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Email es obligatorio') 
+        if not subject:
+            errors.append('asunto es obligatorio') 
+        if not content:
+            errors.append('contenido es obligatorio') 
+
+        if len(errors) == 0:
+            db, c = get_db()
+            c.execute('INSERT INTO email (email, subject, content) VALUES(%s, %s, %s)', (email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
+        else:
+            for error in errors: 
+                flash(errors)
+    return render_template('mails/create.html')
+
+def send(to, subject, content):
+    sg = sendgrid.sendGriAPIclient(api_key=current_app.config['SENDGRID_KEY'])
