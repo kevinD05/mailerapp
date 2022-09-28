142295568c8f348bb8310e1e8170a3ec4294c2b6
unknown <alexanderkevindiaz05@gmail.com>
2022-09-28 13:25:16 -0600

    tercer commit

--- app/mail.py
+++ app/mail.py
@@ -1,6 +1,10 @@
+
 from flask import (
-    Blueprint, render_template, request, flash, redirect, url_for
+    Blueprint, render_template, request, flash, redirect, url_for, current_app
 )
+import sendgrid 
+from sendgrid.helpers.mail import *
+
 
 from app.db import get_db
 
@@ -32,6 +36,7 @@ def create():
             errors.append('contenido es obligatorio') 
 
         if len(errors) == 0:
+           # send(email, subject, content)
             db, c = get_db()
             c.execute('INSERT INTO email (email, subject, content) VALUES(%s, %s, %s)', (email, subject, content))
             db.commit()
@@ -43,4 +48,10 @@ def create():
     return render_template('mails/create.html')
 
 def send(to, subject, content):
-    sg = sendgrid.sendGriAPIclient(api_key=current_app.config['SENDGRID_KEY'])
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+    from_email = Email(current_app.config['FROM_EMAIL'])
+    to_email = To(to)
+    content = content('text/plain', content)
+    mail = Mail(from_email, to_email, content)
+    response =  sg.client.send.post(request_body=mail.get())
+    print(response)
